56f7e740e443e5f9106ac211bf4134e536592f13
Tónga <gastongracis@gmail.com>
2023-04-23 22:57:27 -0300

    in english

--- app.js
+++ app.js
@@ -3,17 +3,63 @@ const https = require("https");
 const http = require("http");
 const axios = require("axios");
 const Email = require("./mailer");
-const fs = require("fs"); // Agregamos el módulo fs para escribir en archivos
+const fs = require("fs");
+const i18n = require("i18n");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+
+// Configurar i18n
+i18n.configure({
+  locales: ["es", "en"],
+  directory: __dirname + "/locales",
+  defaultLocale: "es",
+  queryParameter: "lang",
+});
+
+// Agregar el middleware de i18n a la cadena de manejo de solicitudes
+app.use(cookieParser());
+app.use(i18n.init);
+
+app.use(function (req, res, next) {
+  res.locals.currentLocale = req.getLocale();
+  next();
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.render("index", { currentLocale: req.cookies.i18n || "es" });
+});
+
+app.get("/change-language/:locale", (req, res) => {
+  const locale = req.params.locale;
+  res.cookie("i18n", locale, { maxAge: 900000, httpOnly: true });
+  req.setLocale(locale);
+  app.locals.currentLocale = locale;
+  res.redirect("/");
+});
+
+app.use((req, res, next) => {
+  const currentLocale = req.cookies.i18n || "es"; // Si no se encuentra la cookie, se establece el valor 'es' por defecto.
+  app.locals.currentLocale = currentLocale;
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Página no encontrada" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
 
 // Configuramos el archivo donde se guardarán los datos
 const archivoContactos = path.join(__dirname, "public", "contactos.html");
@@ -25,40 +71,6 @@ if (!fs.existsSync(archivoContactos)) {
     "<tr><th>Nombre</th><th>Correo</th><th>Mensaje</th></tr>"
   );
 }
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// Certificate for Domain 1
-const privateKey1 = fs.readFileSync(
-  "/etc/letsencrypt/live/www.gastongracis.dev/privkey.pem",
-  "utf8"
-);
-const certificate1 = fs.readFileSync(
-  "/etc/letsencrypt/live/www.gastongracis.dev/cert.pem",
-  "utf8"
-);
-const ca1 = fs.readFileSync(
-  "/etc/letsencrypt/live/www.gastongracis.dev/chain.pem",
-  "utf8"
-);
-const credentials1 = {
-  key: privateKey1,
-  cert: certificate1,
-  ca: ca1,
-};
-// Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials1, app);
-
-httpServer.listen(80, () => {
-  console.log("HTTP Server running on port 80");
-});
-
-httpsServer.listen(443, () => {
-  console.log("HTTPS Server running on port 443");
-});
 
 const oEmail = new Email({
   host: "smtp.hostinger.com",
@@ -99,13 +111,4 @@ app.post("/contacto", (req, res, next) => {
 
   oEmail.enviarCorreo(correo);
   res.status(200).json({ message: "Mensaje enviado correctamente" });
-});
-
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Página no encontrada" });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
